006649d07b1df969c167c74f12466a77ac15aa6d
desfate <yujieworking@gmail.com>
2019-01-24 22:37:24 +0800

    消息公告工具库  第一次提交

--- app/src/main/java/com/defate/mac/androidcommondemo/MainActivity.kt
+++ app/src/main/java/com/defate/mac/androidcommondemo/MainActivity.kt
@@ -2,9 +2,11 @@ package com.defate.mac.androidcommondemo
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.defate.mac.common_android.NotificationTools
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        NotificationTools.getNotificationCompatBuilder(this).setContentTitle("xxx") //设置通知栏标题
+            .setContentText("xxx") //设置通知栏显示内容
+            .setPriority(NotificationCompat.PRIORITY_MAX) //设置通知优先级
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setAutoCancel(true); //设置这个标志当用户单击面板就可以将通知取消
+
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
--- common_android/src/main/java/com/defate/mac/common_android/NotificationTools.java
+++ common_android/src/main/java/com/defate/mac/common_android/NotificationTools.java
@@ -1,4 +1,44 @@
 package com.defate.mac.common_android;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+/**
+ * android notification tools class
+ * 安卓消息机制的工具类 实现消息栏通知功能的整理
+ *
+ * notification type    1. normal message  2. message with click listener  3. show download progress
+ * 一般的消息类型  1. 普通文字消息通知   2.消息带点击功能  3.展示下载进度小消息通知
+ *
+ * use step
+ * 配置Notification.Builder
+ * 获取状态栏通知管理NotificationManager
+ * 绑定Notification，发送通知请求。
+ */
 public class NotificationTools {
+
+    /**
+     * 根据当前sdk版本生成notificationCompat
+     * @param context
+     * @return
+     */
+    public static NotificationCompat.Builder getNotificationCompatBuilder(Context context){
+        //判断是否是8.0Android.O
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel chan1 = new NotificationChannel("static", "Primary Channel", NotificationManager.IMPORTANCE_HIGH);
+            //获取状态通知栏管理
+            NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(chan1);
+            return new NotificationCompat.Builder(context, "static");
+        } else {
+            return new NotificationCompat.Builder(context);
+        }
+    }
+
+
+
+
 }
