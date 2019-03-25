0ebf899269ac3488e9be3986e9aa049bde08b61c
yu.jie <yujieworking@gmail.com>
2019-03-25 20:24:13 +0800

    2019/03/25 rxjava相关了解  以及未来在android上的应用

---
+++ app/src/main/java/com/defate/mac/androidcommondemo/rxjavas/RxjavaWithAndroid.kt
@@ -0,0 +1,25 @@
+package com.defate.mac.androidcommondemo.rxjavas
+
+/**
+ * 用于展示rxjava在android中应用的场景
+ * https://blog.csdn.net/carson_ho/article/details/79168723
+ */
+class RxjavaWithAndroid{
+
+    /**
+     * 这里有几类
+     *
+     * 1.网络请求轮询  （与创建操作符相关）
+     *
+     * 2.网络请求嵌套回调 （与变换操作符相关）
+     *
+     * 3.合并数据源  获取缓存数据源  联合判断 （组合 合并操作符）
+     *
+     * 4.线程操作  请求错误重试  轮询  （功能操作符）
+     *
+     * 5.功能防抖  联想搜索请求优化  （过滤操作符）
+     *
+     * 6.背压
+     */
+
+}
